dac3d80540582e4089cdad9a48114b7a46ed4a10
Shindoree <166209655+Shindoree@users.noreply.github.com>
2024-04-15 16:00:39 +0800

    Changed the default font style to Montserrat Regular 12

--- Wyteboard V2/LoginForm.Designer.vb
+++ Wyteboard V2/LoginForm.Designer.vb
@@ -26,9 +26,11 @@ Partial Class LoginForm
         '
         'LoginForm
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(15.0!, 33.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1258, 664)
+        Me.Font = New System.Drawing.Font("Montserrat", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(5, 5, 5, 5)
         Me.Name = "LoginForm"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "LoginForm"

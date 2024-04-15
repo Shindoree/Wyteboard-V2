21ca092253137a232db217ea5a16815c57bbf2ba
AnMalaque <Alexandermalaque01@gmail.com>
2024-04-15 17:13:43 +0800

    ForgotPasswordForm

---
+++ Wyteboard V2/ForgotPasswordForm.vb
@@ -0,0 +1,9 @@
+﻿Public Class ForgotPasswordForm
+    Private Sub Label7_Click(sender As Object, e As EventArgs) Handles Label7.Click
+
+    End Sub
+
+    Private Sub Label8_Click(sender As Object, e As EventArgs) Handles Label8.Click
+
+    End Sub
+End Class

3f0dbe36a142339c5eb6e8d9684a993bff716d52
AnMalaque <Alexandermalaque01@gmail.com>
2024-04-16 14:00:56 +0800

    Added Professor Form

--- Wyteboard V2/My Project/Resources.Designer.vb
+++ Wyteboard V2/My Project/Resources.Designer.vb
@@ -73,6 +73,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property icons8_add_32() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("icons8-add-32", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property icons8_course_32() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("icons8-course-32", resourceCulture)
---
+++ Wyteboard V2/ProfessorForm.vb
@@ -0,0 +1,3 @@
+﻿Public Class ProfessorForm
+
+End Class
